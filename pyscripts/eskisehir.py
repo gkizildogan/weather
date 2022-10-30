b830dd42ba421143e7d4c85fe9266f9d35027107
gkizildogan <goksu.kizildogan@gmail.com>
2022-10-30 19:01:15 +0300

    Added january history for test purposes

--- pyscripts/eskisehir.py
+++ pyscripts/eskisehir.py
@@ -5,8 +5,8 @@ lat = 39.7667
 lot = 30.5256
 
 #Start and end dates in YYYY-MM-DD format (ISO 8601)
-startdate = "2010-01-01"
-enddate = "2021-12-31"
-csvname = "weather_history.csv"
+startdate = "2022-01-01"
+enddate = "2022-01-31"
+csvname = "weather_jun.csv"
 
 csv_writer(csvname,lat,lot,startdate,enddate)
